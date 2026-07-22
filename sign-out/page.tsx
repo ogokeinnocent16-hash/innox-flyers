810ac1f0145ec5a5a1a8fae2020a3cd463ecc6bf
Innocent <ogokeinnocent16@gmail.com>
2026-07-22 02:17:11 +0100

    add signout page

---
+++ sign-out/page.tsx
@@ -0,0 +1,17 @@
+"use client"
+import { useClerk } from "@clerk/nextjs";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+
+export default function SignOutPage() {
+  const { signOut } = useClerk();
+  const router = useRouter();
+
+  useEffect(() => {
+    signOut().then(() => {
+      router.push("/"); // after logout, go back to homepage
+    });
+  }, [signOut, router]);
+
+  return <p style={{color:'#fff', textAlign:'center', paddingTop:'50px', fontSize:'20px'}}>Signing out...</p>
+}
